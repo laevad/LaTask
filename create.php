8ddea29083b5f685274efae6580ed0d94c80a7b3
laevad <aldave.tabian@ustp.edu.ph>
2022-01-25 09:18:22 +0800

    fix in creating product

--- create.php
+++ create.php
@@ -26,19 +26,20 @@ if(is_post_request()){
 <div class=" container">
     <a class="btn btn-secondary btn-outline-warning mt-3 ml-3 mb-3" href="<?php echo url_for('index.php'); ?>" >&laquo; Back to Homepage</a>
     <h1 class="text-center text-secondary font-weight-bolder">Add Product</h1>
+    <?php echo display_errors($errors); ?>
     <form method="post" action="#" autocomplete="off" enctype="multipart/form-data">
         <div class="modal-body">
             <div class="form-group">
                 <label for="name">Name</label>
-                <input type="text" required class="form-control" id="name" name="name"  placeholder="Enter Name">
+                <input type="text" required class="form-control" id="name" name="name" value="<?php echo $item['name'] ?? ""; ?>"  placeholder="Enter Name">
             </div>
             <div class="form-group">
                 <label for="price">Price</label>
-                <input type="number" required class="form-control" id="price" name="price"  placeholder="Enter Price">
+                <input type="number" required class="form-control" id="price" name="price" value="<?php echo $item['price'] ?? ""; ?>"  placeholder="Enter Price">
             </div>
             <div class="form-group">
                 <label for="description">Description</label>
-                <textarea class="form-control" id="description" rows="3" name="description"></textarea>
+                <textarea class="form-control" id="description" rows="3" name="description"><?php echo $item['description'] ?? ""; ?></textarea>
             </div>
             <input type="file" name="file" class="file" accept="image/*">
             <div class="input-group form-group">
