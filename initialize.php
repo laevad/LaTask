0309ecc7baaf86fdcad4c04e1cda89765f021b71
laevad <djaldave@gmail.com>
2022-07-17 17:42:24 +0800

    change

--- initialize.php
+++ initialize.php
@@ -1,9 +1,10 @@
 <?php
 ob_start(); 
 session_start(); 
-$public_end = strpos($_SERVER['SCRIPT_NAME'], '/LaTask2022') + 7; // project folder should be LaTask
+$public_end = 0; // project folder should be LaTask
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 define("WEB", $doc_root);
+var_dump(WEB);
 require_once('query_function.php');
 require_once('validation_functions.php');
 require_once 'functions.php';
